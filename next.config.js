1f8580f89b996bf25bd7d1054dd00caeeadbe779
francesco.miscia <fra.miscia@gmail.com>
2022-04-22 16:09:07 +0200

    Proxy serve enabled in production build

--- next.config.js
+++ next.config.js
@@ -1,17 +1,14 @@
 /** @type {import('next').NextConfig} */
-const isDevelopment = process.env.NODE_ENV !== "production"
-const rewritesConfig = isDevelopment
-    ? [
-          {
-              source: "/api/:path*",
-              destination: "https://dev-api.confidence.org/v2/:path*",
-          },
-      ]
-    : []
+const rewritesConfig = [
+    {
+        source: "/api/:path*",
+        destination: "https://dev-api.confidence.org/v2/:path*",
+    },
+]
 
 const nextConfig = {
     reactStrictMode: true,
-    rewrites: async() => rewritesConfig
+    rewrites: async () => rewritesConfig,
 }
 
 module.exports = nextConfig
